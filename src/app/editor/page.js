ae8e887a5fcdc91750d138ca664b1c70ad49bc62
rinaldo <rinaldobenaccetta@hotmail.com>
2023-05-09 20:20:25 +0200

    Add editor page

---
+++ src/app/editor/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Editor = () => {
+    return (
+        <div>
+            <h1>Page Editor</h1>
+            <p>Bienvenue sur la page Editor.</p>
+        </div>
+    );
+};
+
+export default Editor;
