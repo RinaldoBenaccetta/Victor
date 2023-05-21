dbfc7c7b7e5fb06f93940a0c71e036b36cd22e99
rinaldo <rinaldobenaccetta@hotmail.com>
2023-05-21 15:57:02 +0200

    Put Menu items of contextMenu in a List element

--- src/components/editor/ContextMenu.js
+++ src/components/editor/ContextMenu.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { Popper, Fade, Paper, MenuItem } from "@mui/material";
+import { Popper, Fade, Paper, MenuItem, List } from "@mui/material";
 import { useRef } from "react";
 
 const ContextMenu = ({ anchorEl, setAnchorEl, selectedText }) => {
@@ -14,58 +14,61 @@ const ContextMenu = ({ anchorEl, setAnchorEl, selectedText }) => {
     const isSingleWord = selectedText && !selectedText.trim().includes(" ");
 
     return (
+        // Popper because Menu is not working well with fake anchorEl
         <Popper open={Boolean(anchorEl)} anchorEl={anchorEl} transition>
             {({ TransitionProps }) => (
                 <Fade {...TransitionProps} timeout={350}>
                     <Paper>
-                        {[
-                            isSingleWord && (
+                        <List>
+                            {[
+                                isSingleWord && (
+                                    <MenuItem
+                                        ref={singleWordFirstRef}
+                                        onClick={handleClose}
+                                        key="synonyme"
+                                        tabIndex={0} // this make menuItem focusable with tab
+                                    >
+                                        synonyme
+                                    </MenuItem>
+                                ),
+                                isSingleWord && (
+                                    <MenuItem
+                                        onClick={handleClose}
+                                        key="antonyme"
+                                        tabIndex={0}
+                                    >
+                                        antonyme
+                                    </MenuItem>
+                                ),
                                 <MenuItem
-                                    ref={singleWordFirstRef}
+                                    ref={notASingleWordFirstRef}
                                     onClick={handleClose}
-                                    key="synonyme"
-                                    tabIndex={0} // this make menuItem focusable with tab
-                                >
-                                    synonyme
-                                </MenuItem>
-                            ),
-                            isSingleWord && (
-                                <MenuItem
-                                    onClick={handleClose}
-                                    key="antonyme"
+                                    key="option1"
                                     tabIndex={0}
                                 >
-                                    antonyme
-                                </MenuItem>
-                            ),
-                            <MenuItem
-                                ref={notASingleWordFirstRef}
-                                onClick={handleClose}
-                                key="option1"
-                                tabIndex={0}
-                            >
-                                Option 1
-                            </MenuItem>,
-                            <MenuItem
-                                onClick={handleClose}
-                                key="option2"
-                                tabIndex={0}
-                                // This make a circular navigation :
-                                // the focus go on the first item when press tab
-                                onKeyDown={e => {
-                                    if (e.key === "Tab") {
-                                        e.preventDefault();
-                                        // focus on right item according to single word or not
-                                        (isSingleWord
-                                            ? singleWordFirstRef
-                                            : notASingleWordFirstRef
-                                        ).current.focus();
-                                    }
-                                }}
-                            >
-                                Option 2
-                            </MenuItem>,
-                        ]}
+                                    Option 1
+                                </MenuItem>,
+                                <MenuItem
+                                    onClick={handleClose}
+                                    key="option2"
+                                    tabIndex={0}
+                                    // This make a circular navigation :
+                                    // the focus go on the first item when press tab
+                                    onKeyDown={e => {
+                                        if (e.key === "Tab") {
+                                            e.preventDefault();
+                                            // focus on right item according to single word or not
+                                            (isSingleWord
+                                                ? singleWordFirstRef
+                                                : notASingleWordFirstRef
+                                            ).current.focus();
+                                        }
+                                    }}
+                                >
+                                    Option 2
+                                </MenuItem>,
+                            ]}
+                        </List>
                     </Paper>
                 </Fade>
             )}
