d712241805183990051047fb7c84d366fba02d5d
rinaldo <rinaldobenaccetta@hotmail.com>
2023-05-21 15:50:32 +0200

    Fine tuning ContextMenu for circular navigation

--- src/components/editor/ContextMenu.js
+++ src/components/editor/ContextMenu.js
@@ -1,8 +1,12 @@
 "use client";
 
 import { Popper, Fade, Paper, MenuItem } from "@mui/material";
+import { useRef } from "react";
 
 const ContextMenu = ({ anchorEl, setAnchorEl, selectedText }) => {
+    const singleWordFirstRef = useRef(null);
+    const notASingleWordFirstRef = useRef(null);
+
     const handleClose = () => {
         setAnchorEl(null);
     };
@@ -17,6 +21,7 @@ const ContextMenu = ({ anchorEl, setAnchorEl, selectedText }) => {
                         {[
                             isSingleWord && (
                                 <MenuItem
+                                    ref={singleWordFirstRef}
                                     onClick={handleClose}
                                     key="synonyme"
                                     tabIndex={0} // this make menuItem focusable with tab
@@ -34,11 +39,31 @@ const ContextMenu = ({ anchorEl, setAnchorEl, selectedText }) => {
                                 </MenuItem>
                             ),
                             <MenuItem
+                                ref={notASingleWordFirstRef}
                                 onClick={handleClose}
                                 key="option1"
                                 tabIndex={0}
                             >
                                 Option 1
+                            </MenuItem>,
+                            <MenuItem
+                                onClick={handleClose}
+                                key="option2"
+                                tabIndex={0}
+                                // This make a circular navigation :
+                                // the focus go on the first item when press tab
+                                onKeyDown={e => {
+                                    if (e.key === "Tab") {
+                                        e.preventDefault();
+                                        // focus on right item according to single word or not
+                                        (isSingleWord
+                                            ? singleWordFirstRef
+                                            : notASingleWordFirstRef
+                                        ).current.focus();
+                                    }
+                                }}
+                            >
+                                Option 2
                             </MenuItem>,
                         ]}
                     </Paper>
