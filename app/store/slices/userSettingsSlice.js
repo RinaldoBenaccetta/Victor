995ed868fc0ff698094f7a739e081e02e2279293
rinaldo <rinaldobenaccetta@hotmail.com>
2023-05-29 17:44:12 +0200

    save open ai key in localstorage

--- app/store/slices/userSettingsSlice.js
+++ app/store/slices/userSettingsSlice.js
@@ -1,17 +1,35 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+// Use AsyncThunk to let the reducer pure : prevent board effects.
+export const setApiKey = createAsyncThunk(
+    "userSettings/setApiKey",
+    async key => {
+        localStorage.setItem("VICTOR_OPENAI_KEY", key);
+        return key;
+    }
+);
 
 const userSettingsSlice = createSlice({
     name: "userSettings",
     initialState: {
         apiKey: "",
     },
-    reducers: {
-        setApiKey: (state, action) => {
+    // If reducer is asynchrone or have boards effect,
+    // put it in extraReducers, otherwise, put it in reducers
+    extraReducers: builder => {
+        builder.addCase(setApiKey.fulfilled, (state, action) => {
             state.apiKey = action.payload;
-        },
+        });
     },
 });
 
-export const userSettingsMethods = userSettingsSlice.actions;
+/**
+ * for exporting reducers
+ */
+// export const userSettingsMethods = userSettingsSlice.actions;
+
+export const userSettingsMethods = {
+    setApiKey,
+};
 
 export default userSettingsSlice;
